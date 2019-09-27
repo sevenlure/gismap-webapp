47298bc15039be7c94b89e9887a9f83d8f4d10d3
sevenlure <01226905092>
2019-09-27 21:41:18 +0700

    update preload font

--- pages/_document.js
+++ pages/_document.js
@@ -38,10 +38,10 @@ class MyDocument extends Document {
           <link rel='stylesheet' href='/static/css/simplebar.min.css' />
           <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
           <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-          <link rel="preload" href="/static/font/HelveticaNeue.otf" type="font/otf" as="font" crossorigin="anonymous" />
-          <link rel="preload" href="/static/font/HelveticaNeue-Light.otf" type="font/otf" as="font" crossorigin="anonymous" />
-          <link rel="preload" href="/static/font/HelveticaNeue-Bold.otf" type="font/otf" as="font" crossorigin="anonymous" />
-          <link rel="preload" href="/static/font/HelveticaNeue-Medium.otf" type="font/otf" as="font" crossorigin="anonymous" />
+          <link rel="preload" href="/static/font/HelveticaNeue.otf" type="font/otf" as="font" crossOrigin="anonymous" />
+          <link rel="preload" href="/static/font/HelveticaNeue-Light.otf" type="font/otf" as="font" crossOrigin="anonymous" />
+          <link rel="preload" href="/static/font/HelveticaNeue-Bold.otf" type="font/otf" as="font" crossOrigin="anonymous" />
+          <link rel="preload" href="/static/font/HelveticaNeue-Medium.otf" type="font/otf" as="font" crossOrigin="anonymous" />
           <script src='/static/js/simplebar.min.js'></script>
         </Head>
         <body style={{ fontFamily: 'myFont', fontWeight: '300' }}>
